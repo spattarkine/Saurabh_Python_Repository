99a081a1dfbfc63f37fb8d516122e49cc02620eb
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-06-19 07:06:58 +0000

    Eight

--- scrabble.py
+++ scrabble.py
@@ -1,36 +1,51 @@
-"Import Wordscore module"
+"Importing wordscore module"
 import wordscore
 
 def run_scrabble(rack):
-    "Run Scrapable Function Definision"    
+    "Scrabble function business logic"
     rack_low = rack.lower()
     rack_letters = list(rack_low)
     valid_words = []
+    valid_rack = True
+    clean_rack = []
+    if (rack_low.count('?') > 1 or  rack_low.count('*')) > 1:
+        valid_rack = False
+        return ("You have entered more than 2 special characters")
 
-    with open("sowpods.txt","r" , encoding="utf-8") as infile:
-        raw_input = infile.readlines()
-        data = [datum.strip('\n') for datum in raw_input]
-
-    for word in data:
-        word_low = word.lower()
-        temp_rack_letters = rack_letters.copy()
-        for letter in word_low:
-            if letter in temp_rack_letters :
-                temp_rack_letters .remove(letter)
-            elif '*' in temp_rack_letters :
-                temp_rack_letters .remove('*')
-            elif '?' in temp_rack_letters :
-                temp_rack_letters .remove('?')
+    elif len(rack_low) > 7 or len(rack_low) < 2:
+        valid_rack = False
+        return ("You have entered more than 7 characters or less than 2")
+    elif all(x.isalpha() or x in ('?', '*') for x in rack_low):
+        valid_rack = True
+    else:
+        valid_rack = False
+        return ('Please enter only alphabets and/or special characters * and ?')
+    if valid_rack:
+        with open("sowpods.txt","r", encoding="utf-8") as infile:
+            raw_input = infile.readlines()
+            data = [datum.strip('\n') for datum in raw_input]
+        for word in data:
+            word_low = word.lower()
+            temp_rack_letters = rack_letters.copy()
+            for letter in word_low:
+                if letter in temp_rack_letters :
+                    temp_rack_letters .remove(letter)
+                elif '*' in temp_rack_letters :
+                    temp_rack_letters .remove('*')
+                elif '?' in temp_rack_letters :
+                    temp_rack_letters .remove('?')
+                else:
+                    break
             else:
-                break
-        else:
-            #valid_words.append([wordscore.score_word(word_low), word_low])
-            valid_words.append([wordscore.score_word(word_low), word_low.upper()])
-            valid_words.sort(reverse = True)
+                clean_rack = ''.join([i for i in rack_low if i.isalpha()])
+                valid_words.append([wordscore.score_word(clean_rack), word_low.upper()])
+        valid_words.sort(reverse = True)
 
-    for entry in valid_words:
-        score = entry[0]
-        word_low = entry[1]
-        print((score, word_low))
-
-    print(data[0:6])
+        for entry in valid_words:
+            score = entry[0]
+            word_low = entry[1]
+            print((score, word_low))
+        print(len(valid_words))
+    else:
+        valid_rack = False
+    
