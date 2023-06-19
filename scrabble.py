4598ef75d60532aa3e6a5a63dd90f867846a61ce
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-06-19 05:07:51 +0000

    K

--- scrabble.py
+++ scrabble.py
@@ -1,17 +1,20 @@
+"Importing wordscore Module"
+
 from wordscore import *
 
 def run_scrabble(rack):
+    "Run Scrapable Function Definision"    
     rack_low = rack.lower()
     rack_letters = list(rack_low)
     valid_words = []
-    
-    with open("sowpods.txt","r") as infile:
+
+    with open("sowpods.txt","r" , encoding="utf-8") as infile:
         raw_input = infile.readlines()
         data = [datum.strip('\n') for datum in raw_input]
-    
+
     for word in data:
         word_low = word.lower()
-        temp_rack_letters = rack_letters.copy() 
+        temp_rack_letters = rack_letters.copy()
         for letter in word_low:
             if letter in temp_rack_letters :
                 temp_rack_letters .remove(letter)
@@ -21,9 +24,9 @@ def run_scrabble(rack):
                 temp_rack_letters .remove('?')
             else:
                 break
-    else:
-        valid_words.append([score_word(word_low), word_low])
-    valid_words.sort(reverse = True)
+        else:
+            valid_words.append([score_word(word_low), word_low])
+            valid_words.sort(reverse = True)
 
     for entry in valid_words:
         score = entry[0]
