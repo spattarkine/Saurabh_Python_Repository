aa10eaf8ae7f1d8dd8636819f14410cd57e87ded
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-06-19 04:35:12 +0000

    Save_One

---
+++ scrabble.py
@@ -0,0 +1,33 @@
+from wordscore import *
+
+def run_scrabble(rack):
+    rack_low = rack.lower()
+    rack_letters = list(rack_low)
+    valid_words = []
+    
+    with open("sowpods.txt","r") as infile:
+        raw_input = infile.readlines()
+        data = [datum.strip('\n') for datum in raw_input]
+    
+    for word in data:
+        word_low = word.lower()
+        temp_rack_letters = rack_letters.copy() 
+        for letter in word_low:
+            if letter in temp_rack_letters :
+                temp_rack_letters .remove(letter)
+            elif '*' in temp_rack_letters :
+                temp_rack_letters .remove('*')
+            elif '?' in temp_rack_letters :
+                temp_rack_letters .remove('?')
+            else:
+                break
+    else:
+        valid_words.append([score_word(word_low), word_low])
+    valid_words.sort(reverse = True)
+
+    for entry in valid_words:
+        score = entry[0]
+        word_low = entry[1]
+        print((score, word_low))
+
+    print("Total number of words:", len(valid_words))
---
+++ wordscore.py
@@ -0,0 +1,33 @@
+"This function is define score for words" 
+def score_word(word_low):
+    "This function is define score for words"
+    scores = {"a": 1,
+              "c": 3, 
+              "b": 3, 
+              "e": 1, 
+              "d": 2, 
+              "g": 2,
+              "f": 4, 
+              "i": 1, 
+              "h": 4, 
+              "k": 5, 
+              "j": 8, 
+              "m": 3,
+              "l": 1, 
+              "o": 1, 
+              "n": 1, 
+              "q": 10, 
+              "p": 3, 
+              "s": 1,
+              "r": 1, 
+              "u": 1, 
+              "t": 1, 
+              "w": 4, 
+              "v": 4, 
+              "y": 4,
+              "x": 8, 
+              "z": 10}
+    total = 0
+    for letter in word_low:
+        total = total + scores[letter]
+    return total
