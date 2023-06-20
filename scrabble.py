176a81e74f9533337c851da97739e96f768ad1aa
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-06-20 14:33:24 +0000

    VV

--- scrabble.py
+++ scrabble.py
@@ -49,7 +49,7 @@ def run_scrabble(rack):
         for entry in valid_words:
             score = entry[1]
             word_low = entry[0]
-            #result = result + (word_low, score)
-        return (result, (word_low,score))
+            result = result + (word_low, score)
+        return result
     else:
         valid_rack = False
