d6467b58bf95e6f814217da5c4c2009119ff9319
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-06-21 00:42:07 +0000

    cnt

--- scrabble.py
+++ scrabble.py
@@ -9,7 +9,7 @@ def run_scrabble(rack):
     #result=[]
     #result_tuple = ()
     valid_rack = True
-    if (rack_low.count('?') > 1 or  rack_low.count('*')) > 1:
+    if (rack_low.count('?')  +  rack_low.count('*')) > 2:
         #If program has more than one wildcard.
         valid_rack = False
         return False
