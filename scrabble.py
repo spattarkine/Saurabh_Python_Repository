c454bead316fa19142d9a6e850cd375c7797ea25
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-06-20 22:42:41 -0700

    AQW

--- scrabble.py
+++ scrabble.py
@@ -14,15 +14,20 @@ def run_scrabble(rack):
         return ("You have entered more than 2 special characters")
 
     elif len(rack_low) > 7 or len(rack_low) < 2:
-        #If length of word if less than two or greater than seven.
+        #Check if length of word if less than two or greater than seven and return a string error message.
         valid_rack = False
         return ("You have entered more than 7 characters or less than 2")
     elif all(x.isalpha() or x in ('?', '*') for x in rack_low):
+        #Check if only one of the wildcard is used ? or * and return true for further processing. 
         valid_rack = True
     else:
+        #All other categories to return generic error and ask user to enter allowed values only.
         valid_rack = False
         return ('Please enter only alphabets and/or special characters * and ?')
     if valid_rack:
+        #If all validations are passed program will execute further.
+        #Read file sowpods.txt and check each word in lower case in a loop.
+        #Remove duplicate word for temp_rack_letters once it has occurred to avoid duplication.
         with open("sowpods.txt","r", encoding="utf-8") as infile:
             raw_input = infile.readlines()
             data = [datum.strip('\n') for datum in raw_input]
@@ -39,7 +44,7 @@ def run_scrabble(rack):
                 else:
                     break 
             else:
-                clean_rack = ''.join([i for i in rack_low if i.isalpha()])
+                clean_rack = ''.join([i for i in temp_rack_letters if i.isalpha()])
                 # removes the special characters from the rack 
                 valid_words.append((wordscore.score_word(clean_rack), word_low.upper()))
                 sorted_scores = sorted(valid_words, key=lambda x: (-x[0], x[1]), reverse=True)
