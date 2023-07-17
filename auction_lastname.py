fe272fea672d3e97968a2bbdfb795dba15b14bfc
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-07-17 23:45:59 +0000

    1645

--- auction_lastname.py
+++ auction_lastname.py
@@ -2,94 +2,25 @@ from random import randint
 from numpy import random
  
 class User:
-    """
-    Represents a user that will click an ad based on a secret probability"""
-    def _init_(self):
-        """ Initializer for a user
-        """
-        self.__probability = random.uniform(0, 1)
-
-    def get_probability(self):
-        """ Returns the secret probability.
-        """
-        return self.__probability
-
+    def __init__(self):
+        self.__click_prob = random.uniform(0, 1)
+    
     def show_ad(self):
-        """ Displays an ad to a user. The user clicks the ad based on their secret probability.
-        """
-
-        # draw a random number to simulate whether the user clicked
-        click_threshold = random.uniform(0, 1)
-
-        # compare the random draw to the secret probability to represent if the user clicked
-        if click_threshold <= self.__probability:
-            return True
-
-        return False
-
+        return random.random() <= self.__click_prob
 
 class Auction:
-    """
-    Represents an auction containing 1 or more rounds.
-   
-    """
-    def _init_(self, users, bidders):
-        """ Initializer for an Auction
-      
-        """
-        self.users = users
-        self.bidders = bidders
-        # initialize all balances to 0
-        self.balances = {bidder: 0 for bidder in bidders}
+    def __init__(self, num_users, num_rounds):
+        self.users = [User() for _ in range(num_users)]
+        self.bidders = [Bidder() for _ in range(num_bidders)]
+        self.num_rounds = num_rounds
 
-    def execute_round(self):
-        """ A method to simulate a single round of an auction and notifies bidders of round results via notify()
-        
-        """
-
-        # select a random user for the round
-        random_user = randint(0, len(self.users))
-
-        bids = {}
-        for bidder in self.bidders:
-           # call each bidder's bid method to place the bid
-            bids[bidder] = bidder.bid(random_user)
-
-        # select the winning bidder and winning price (second highest bid)
-        highest_bid = 0
-        winning_price = 0
-
-        for bidder, bid_value in bids.items():
-        # evaluate the bid to determine if it is the highest
-        # the highest bidder wins the round
-            if bid_value > highest_bid:
-            # set the winning bid price to the previous high bid
-                winning_price = highest_bid
-                highest_bid = bid_value
-            # this case is for when the bid price < highest bid but > the current winning price
-            elif bid_value > winning_price:
-                winning_price = bid_value
-
-        # tie-breaking logic
-        winning_bidders = []
-        # create a list of all bidders who bid the highest price
-        for bidder in self.bidders:
-            if bids[bidder] == highest_bid:
-                winning_bidders.append(bidder)
-                # randomly select the winning bidder from this list
-                winning_bidder_index = (0 if len(winning_bidders) == 1 else randint(0, len(winning_bidders)))
-                winning_bidder = winning_bidders[winning_bidder_index]
-
-        # show the ad to the user
-        ad_result = self.users[random_user].show_ad()
-
-        for bidder in self.bidders:
-            if bidder == winning_bidder:
-            # send notification to the winning bidder and update the balance
-                bidder.notify(True, winning_price, ad_result)
-                self.balances[bidder] -= winning_price
-        if ad_result:
-            self.balances[bidder] += 1
-        else:
-            # send notification to the losing bidders
-            bidder.notify(False, winning_price, None)
+    def run(self):
+        for round in range(self.num_rounds):
+            user = random.choice(self.users)
+            bids = [(bidder, bidder.bid(user.id)) for bidder in self.bidders]
+            winner, winning_bid = max(bids, key=lambda x: x[1])
+            second_highest_bid = max(bid for _, bid in bids if bid < winning_bid)
+            winning_price = second_highest_bid if second_highest_bid else 0
+            clicked = user.show_ad()
+            for bidder, bid in bids:
+                bidder.notify(bidder == winner, winning_price, clicked)
--- bidder_lastname.py
+++ bidder_lastname.py
@@ -1,116 +1,13 @@
+import random
 class Bidder:
-    """
-    Represents an bidder in the auction
-    """
-
-    def _init_(self, num_users, num_rounds):
-        """ Initializer for a bidder
-
-        """
-
-        self.num_users = num_users
-        self.num_rounds = num_rounds
-        self.bidding_round = 0
-
-        # clearing prices for each user
-        self.user_clearing_prices = {i: [] for i in range(num_users)}
-        # dictionary mapping users to number of bids won for a user
-        self.user_wins = {i: 0 for i in range(num_users)}
-        # dictionary mapping the number of clicks for a user
-        self.user_clicks = {i: 0 for i in range(num_users)}
-        # the ratio of clicks to winning bids
-        self.user_click_ratio = {i: 0. for i in range(num_users)}
-        # current round number
-        self.current_round_user_id = -1
-
-        # percentage of users to explore before starting exploitation
-        self.exploration_users = .25
-        # if there are a small number of users relative to the number of rounds, explore all users
-        if self.num_users / self.num_rounds <= .2:
-            self.exploration_users = 1
-            # bid price during exploration
-            self.exploration_bid_price = 1.
-            self.default_bid_price = 0.5
-            # next bid for a specific user
-        self.next_bid = {i: self.default_bid_price for i in range(num_users)}
-
-        # set max bid amounts for high/med/low confidence bids
-        self.high_bid_max = 0.99
-        self.med_bid_max = 0.65
-        self.low_bid_max = 0.1
-
-        # adders applied to bids based on the last clearing price
-        self.large_adder = 0.01
-        self.small_adder = 0.005
-
-        # phase of algorithm - explore or exploit
-        self.phase = 'explore'
-
+    def __init__(self):
+        self.__balance = 0
+    
     def bid(self, user_id):
-        """ Place a bid for the right to show an ad to a specific user
- 
-        """
-
-        self.bidding_round += 1
-        self.current_round_user_id = user_id
-        bid_price = self.default_bid_price
-
-        # exploit if in exploitation phase, or the user has been seen before
-        if self.user_wins[self.current_round_user_id] > 0 or self.phase == 'exploit':
-            bid_price = self.next_bid[self.current_round_user_id]
-        else:
-        # if in exploration phase, try to win the round
-            bid_price = self.exploration_bid_price
-
-        return bid_price
-
+        return random.uniform(0, self.__balance + 1)
+    
     def notify(self, auction_winner, price, clicked):
-        """ Notify the bidder of auction results
-
-        """
-
-        # dynamically adjust the exploration bid based on the last bid price
-        self.exploration_price = max(self.exploration_price, min(price + (price * self.small_adder), self.high_bid_max))
-
-        # update the list of clearing prices for this user
-        self.user_clearing_prices[self.current_round_user_id].append(price)
-        #print(f"Winning bid price was {price}")
-
         if auction_winner:
-            # update the list of total winning bids for this user
-            self.user_wins[self.current_round_user_id] += 1
-
-        if clicked:
-            # update the number of clicks for this user
-            self.user_clicks[self.current_round_user_id] += 1
-
-        else:
-            pass
-
-        # calculate the click ratio for the user
-        if self.user_wins[self.current_round_user_id] > 0:
-            self.user_click_ratio[self.current_round_user_id] = self.user_clicks[self.current_round_user_id] 
-            #self.user_wins[self.current_round_user_id]
-        else:
-            self.user_click_ratio[self.current_round_user_id] = 0
-
-        # calculate the next bid for this user based on history
-        if self.user_click_ratio.get(self.current_round_user_id, 0) > 0.8:
-         #   if the user is likely to click
-         # use the last clearing price for this user plus a large adder
-            self.next_bid[self.current_round_user_id] = min(
-            self.user_clearing_prices[self.current_round_user_id][-1]+ (self.user_clearing_prices[self.current_round_user_id][-1] * self.large_adder), self.high_bid_max,)
-        elif self.user_click_ratio.get(self.current_round_user_id, 0) > 0.5:
-        # if the user is somewhat likely to click
-         # use the last clearing price for this user plus a small adder
-            self.next_bid[self.current_round_user_id] = min(
-            self.user_clearing_prices[self.current_round_user_id][-1]+ (self.user_clearing_prices[self.current_round_user_id][-1] * self.small_adder), self.med_bid_max,)
-        else:
-             # if the user is not likely to click
-             # use a static value
-            self.next_bid[self.current_round_user_id] = min(self.user_clearing_prices[self.current_round_user_id][-1]- (self.user_clearing_prices[self.current_round_user_id][-1] * self.large_adder),self.high_bid_max,)
-
-            # determine the phase based on the number of users who've been explored
-        if len(self.user_wins.keys()) / self.num_users >= self.exploration_users or len(self.user_wins.keys()) == self.num_users:
-            self.phase = 'exploit'
-            
+            self.__balance -= price
+            if clicked:
+                self.__balance += 1
