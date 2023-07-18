3042536dc05e4b7947c321c0dd4cc290828fefa0
spattarkine <134421380+spattarkine@users.noreply.github.com>
2023-07-18 00:03:16 +0000

    1703

--- auction_lastname.py
+++ auction_lastname.py
@@ -9,18 +9,18 @@ class User:
         return random.random() <= self.__click_prob
 
 class Auction:
-    def __init__(self, num_users, num_rounds):
-        self.users = [User() for _ in range(num_users)]
-        self.bidders = [Bidder() for _ in range(num_bidders)]
-        self.num_rounds = num_rounds
+    def __init__(self, users, bidders):
+        self.users = users
+        self.bidders = bidders
 
-    def run(self):
-        for round in range(self.num_rounds):
-            user = random.choice(self.users)
-            bids = [(bidder, bidder.bid(user.id)) for bidder in self.bidders]
-            winner, winning_bid = max(bids, key=lambda x: x[1])
-            second_highest_bid = max(bid for _, bid in bids if bid < winning_bid)
-            winning_price = second_highest_bid if second_highest_bid else 0
-            clicked = user.show_ad()
-            for bidder, bid in bids:
-                bidder.notify(bidder == winner, winning_price, clicked)
+    def execute_round(self):
+        user = random.choice(self.users)
+        bids = [bidder.bid(user_id=user_id) for user_id, bidder in enumerate(self.bidders)]
+        winner_idx = max(range(len(self.bidders)), key=lambda i: bids[i])
+        winners = [i for i in range(len(self.bidders)) if bids[i] == bids[winner_idx]]
+        winning_price = max(bids)
+        if len(winners) > 1:
+            winning_price = bids[winner_idx]
+        clicked = user.show_ad()
+        for i, bidder in enumerate(self.bidders):
+            bidder.notify
